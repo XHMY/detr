6a34b020343de5d6e938224cd067dd6f16ed9d32
Yifan Zeng <zyf6@outlook.com>
2024-03-13 11:27:33 -0700

    fix HF model bug

--- engine.py
+++ engine.py
@@ -33,7 +33,7 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
         with autocast():
-            outputs = model(samples)
+            outputs = model(samples.tensors)
             loss_dict = criterion(outputs, targets)
             weight_dict = criterion.weight_dict
             losses = sum(loss_dict[k] * weight_dict[k] for k in loss_dict.keys() if k in weight_dict)
--- models/detr.py
+++ models/detr.py
@@ -219,6 +219,10 @@ class SetCriterion(nn.Module):
              targets: list of dicts, such that len(targets) == batch_size.
                       The expected keys in each dict depends on the losses applied, see each loss' doc
         """
+        if "pred_logits" not in outputs.keys() and "logits" in outputs.keys():
+            # Map huggingface model output to detectron2 output format
+            outputs["pred_logits"] = outputs["logits"]
+
         outputs_without_aux = {k: v for k, v in outputs.items() if k != 'aux_outputs'}
 
         # Retrieve the matching between the outputs of the last layer and the targets
